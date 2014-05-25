023a467dcef82df9f1a4310b6f0742b394eb90f0
Vlada <d0vlaj@yahoo.com>
2014-05-25 20:27:18 +0200

    fixed previous faulty commit; hopefully it will be ok now

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+##-----------cachematrix.r--------------------
+##this file contains a set of functions which
+##cache the inverse of a matrix. This way, a user
+##can simply access the cached inverse as opposed
+##to calculating the inverse each time
 
-## Write a short comment describing this function
+## this function creates a list which joins the matrix and its inverse. 
+## The matrix can be accessed or changed by using 'get' and 'set', and its
+## inverse can be accessed or changed by using 'getInverse' and 'setInverse'
+##
+## calling makeCacheMatrix on a matrix just initializes this list; the inverse
+## is set to NULL.
+## cacheSolve with this list as its parameter needs to be called in order to
+## fill out the inverse.
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() {
+        x
+    }
+    
+    setInverse <- function(inv) {
+        inverse <<- inv
+    }
+    
+    getInverse <- function() {
+        inverse
+    }
+    
+    list (set = set, 
+        get = get, 
+        setInverse = setInverse, 
+        getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## this function gets the cached inverse from the above list if it is already set,
+## or calculates the inverse and sets it into the above list.
+## Arguments:
+## x - matrix for which we want to get the inverse
+## ... - further arguments passed to or from other methods
+## Return value:
+## inverse of matrix x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    inverse <- x$getInverse()
+    
+    if (!is.null(inverse)) {
+        message("getting cached data")
+        return (inverse)
+    }
+    
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setInverse(inverse)
+    
+    inverse
 }
